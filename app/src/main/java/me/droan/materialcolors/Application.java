174dcb02c6c63002be6980de82a7de8e3dfb8a88
Droan Malik <droanmalik@gmail.com>
2016-05-31 15:03:45 +0530

    removed fabric, added backward compatibility upto api 17

--- app/src/main/java/me/droan/materialcolors/Application.java
+++ app/src/main/java/me/droan/materialcolors/Application.java
@@ -1,8 +1,6 @@
 package me.droan.materialcolors;
 
 import android.content.Context;
-import com.crashlytics.android.Crashlytics;
-import io.fabric.sdk.android.Fabric;
 
 public class Application extends android.app.Application {
 
@@ -16,7 +14,7 @@ public class Application extends android.app.Application {
   @Override public void onCreate() {
     super.onCreate();
     if (turnOnFabric) {
-      Fabric.with(this, new Crashlytics());
+
     }
     context = getApplicationContext();
   }
